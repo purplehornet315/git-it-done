e300ae70bc7c69af4e534ec345540af8bc342bd9
Taylor Leverette <kyle_taylor315@hotmail.com>
2022-05-28 11:11:20 -0500

    buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,7 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
+var languageButtonEl= document.querySelector("#language-buttons")
 
 
 var getUserRepos = function(user){
@@ -91,5 +91,30 @@ var displayRepos = function(repos, searchTerm){
     }
 };
 
+var getFeaturedRepos = function(language){
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language);
+            });
+            
+        }else{
+            alert("Error:Github User not Found");
+        }
+    });
+};
+
+var buttonClickHandler = function(event){
+    event.preventDefault();
+    var language = event.target.getAttribute("data-language");
+    if(language){
+        getFeaturedRepos(language);
+        // clear old content
+        repoContainerEl.textContent="";
+    }
+}
 
 userFormEl.addEventListener("submit",formSubmitHandler);
+
+languageButtonEl.addEventListener("click",buttonClickHandler);
